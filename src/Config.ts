8a492c3b32eaeb567f4fac77294b9f0a6e367e69
Firebat <bbfarmcontact@gmail.com>
2021-09-19 01:35:40 +0900

    init

---
+++ src/Config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    chainId: 56,
+    rpc: "https://bsc-dataseed.binance.org",
+
+    contracts: {
+        Cake: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+        CakeNFTStore: "0x1DC22203157529184af7C63d42f632b2BEE317Bd",
+    },
+};
