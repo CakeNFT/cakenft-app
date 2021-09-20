5c31c99e1c01e89769ae99d2043d86f0f00a001f
Firebat <bbfarmcontact@gmail.com>
2021-09-20 18:34:41 +0900

    Add Create Page

--- src/view/Create.ts
+++ src/view/Create.ts
@@ -1,5 +1,5 @@
 import { DomNode, el } from "@hanul/skynode";
-import { View, ViewParams } from "skyrouter";
+import { SkyRouter, View, ViewParams } from "skyrouter";
 import Layout from "./Layout";
 
 export default class Create implements View {
@@ -8,6 +8,30 @@ export default class Create implements View {
 
     constructor() {
         Layout.current.content.append(this.container = el(".create-view",
+            el("main",
+                el("h1", "Create NFTs"),
+                el("section",
+                    el("a",
+                        el("h3", "Create new item"),
+                        el("p", "Create new NFT Simple & Easy"),
+                        { click: () => SkyRouter.go("/create/one") },
+                    ),
+                ),
+                el("section",
+                    el("h2", "Create new smart contract"),
+                    el("p", "You need ", el("a", "Base URL")),
+                    el("a",
+                        el("h3", "Create Deployer Mint NFT"),
+                        el("p", "Mint deployer only"),
+                        { click: () => SkyRouter.go("/create/deployer-mint-nft") },
+                    ),
+                    el("a",
+                        el("h3", "Create User Mint NFT"),
+                        el("p", "Mint everyone"),
+                        { click: () => SkyRouter.go("/create/user-mint-nft") },
+                    ),
+                ),
+            ),
         ));
     }
 
